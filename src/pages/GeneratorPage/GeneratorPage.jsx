4ebc0665fc7dea78d4b0dcabf69682e41590f58c
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2024-11-17 13:43:44 +0200

    add generator page

---
+++ src/pages/GeneratorPage/GeneratorPage.jsx
@@ -0,0 +1,8 @@
+import style from './GeneratorPage.module.css';
+import { useState } from 'react';
+
+const GeneratorPage = ({}) => {
+  return <div>GeneratorPage</div>;
+};
+
+export default GeneratorPage;
